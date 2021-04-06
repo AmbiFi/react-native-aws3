f9cbf4553ba9d570e30e38bacf343f0c656cee87
g-ambifi <gt.ambifi@gmail.com>
2021-04-05 17:42:00 -0700

    Session token support

--- src/S3Policy.js
+++ src/S3Policy.js
@@ -16,101 +16,116 @@ const AWS_ALGORITHM = "AWS4-HMAC-SHA256";
 const DEFAULT_SUCCESS_ACTION_STATUS = "201";
 
 const assert = (object, message) => {
-  if (null == object) throw new Error(message);
+	if (null == object) throw new Error(message);
 }
 
 export class S3Policy {
-  static generate(options) {
-    options || (options = {});
+	static generate(options) {
+		options || (options = {});
 
-    assert(options.key, "Must provide `key` option with the object key");
-    assert(options.bucket, "Must provide `bucket` option with your AWS bucket name");
-    assert(options.contentType, "Must provide `contentType` option with the object content type");
-    assert(options.region, "Must provide `region` option with your AWS region");
-    assert(options.date, "Must provide `date` option with the current date");
-    assert(options.accessKey, "Must provide `accessKey` option with your AWSAccessKeyId");
-    assert(options.secretKey, "Must provide `secretKey` option with your AWSSecretKey");
+		assert(options.key, "Must provide `key` option with the object key");
+		assert(options.bucket, "Must provide `bucket` option with your AWS bucket name");
+		assert(options.contentType, "Must provide `contentType` option with the object content type");
+		assert(options.region, "Must provide `region` option with your AWS region");
+		assert(options.date, "Must provide `date` option with the current date");
+		assert(options.accessKey, "Must provide `accessKey` option with your AWSAccessKeyId");
+		assert(options.secretKey, "Must provide `secretKey` option with your AWSSecretKey");
 
-    const date = options.date;
-    const timeDelta = options.timeDelta || 0;
-    const policyExpiresIn = FIVE_MINUTES - timeDelta;
-    const expirationDate = new Date(date.getTime() + policyExpiresIn);
+		const date = options.date;
+		const timeDelta = options.timeDelta || 0;
+		const policyExpiresIn = FIVE_MINUTES - timeDelta;
+		const expirationDate = new Date(date.getTime() + policyExpiresIn);
 
-    const policyParams = {
-      ...options,
-      acl: options.acl || AWS_ACL,
-      algorithm: AWS_ALGORITHM,
-      amzDate: dateToString(date, 'amz-iso8601'),
-      yyyymmddDate: dateToString(date, 'yyyymmdd'),
-      expirationDate: dateToString(expirationDate, 'iso8601'),
-      successActionStatus: String(options.successActionStatus || DEFAULT_SUCCESS_ACTION_STATUS),
-    }
+		let policyParams = {
+			...options,
+			acl: options.acl || AWS_ACL,
+			algorithm: AWS_ALGORITHM,
+			amzDate: dateToString(date, 'amz-iso8601'),
+			yyyymmddDate: dateToString(date, 'yyyymmdd'),
+			expirationDate: dateToString(expirationDate, 'iso8601'),
+			successActionStatus: String(options.successActionStatus || DEFAULT_SUCCESS_ACTION_STATUS),
+		}
 
-    policyParams.credential = [
-      policyParams.accessKey,
-      policyParams.yyyymmddDate,
-      policyParams.region,
-      AWS_SERVICE_NAME,
-      AWS_REQUEST_POLICY_VERSION
-    ].join('/');
+		if (options.sessionToken) {
+			policyParams.sessionToken = options.sessionToken;
+		}
 
-    const policy = formatPolicyForEncoding(policyParams);
-    const base64EncodedPolicy = getEncodedPolicy(policy);
-    const signature = getSignature(base64EncodedPolicy, policyParams);
+		policyParams.credential = [
+			policyParams.accessKey,
+			policyParams.yyyymmddDate,
+			policyParams.region,
+			AWS_SERVICE_NAME,
+			AWS_REQUEST_POLICY_VERSION
+		].join('/');
 
-    return formatPolicyForRequestBody(base64EncodedPolicy, signature, policyParams);
-  }
+		const policy = formatPolicyForEncoding(policyParams);
+		const base64EncodedPolicy = getEncodedPolicy(policy);
+		const signature = getSignature(base64EncodedPolicy, policyParams);
+
+		return formatPolicyForRequestBody(base64EncodedPolicy, signature, policyParams);
+	}
 }
 
 const formatPolicyForRequestBody = (base64EncodedPolicy, signature, options) => {
-  return {
-    "key": options.key,
-    "acl": options.acl,
-    "success_action_status": options.successActionStatus,
-    "Content-Type": options.contentType,
-    "X-Amz-Credential": options.credential,
-    "X-Amz-Algorithm": options.algorithm,
-    "X-Amz-Date": options.amzDate,
-    "Policy": base64EncodedPolicy,
-    "X-Amz-Signature": signature,
-  }
+	let policyForRequestBody = {
+		"key": options.key,
+		"acl": options.acl,
+		"success_action_status": options.successActionStatus,
+		"Content-Type": options.contentType,
+		"X-Amz-Credential": options.credential,
+		"X-Amz-Algorithm": options.algorithm,
+		"X-Amz-Date": options.amzDate,
+		"Policy": base64EncodedPolicy,
+		"X-Amz-Signature": signature,
+	}
+
+	if (options.sessionToken) {
+		policyForRequestBody['X-Amz-Security-Token'] = options.sessionToken;
+	}
+
+	return policyForRequestBody;
 }
 
 const formatPolicyForEncoding = (policy) => {
-  return {
-    "expiration": policy.expirationDate,
-    "conditions": [
-       {"bucket": policy.bucket},
-       {"key": policy.key},
-       {"acl": policy.acl},
-       {"success_action_status": policy.successActionStatus},
-       {"Content-Type": policy.contentType},
-       {"x-amz-credential": policy.credential},
-       {"x-amz-algorithm": policy.algorithm},
-       {"x-amz-date": policy.amzDate}
-    ]
-  }
+	let formattedPolicy = {
+		"expiration": policy.expirationDate,
+		"conditions": [
+			{ "bucket": policy.bucket },
+			{ "key": policy.key },
+			{ "acl": policy.acl },
+			{ "success_action_status": policy.successActionStatus },
+			{ "Content-Type": policy.contentType },
+			{ "x-amz-credential": policy.credential },
+			{ "x-amz-algorithm": policy.algorithm },
+			{ "x-amz-date": policy.amzDate }
+		]
+	}
+
+	if (policy.sessionToken) {
+		formattedPolicy.conditions.push({ 'x-amz-security-token': policy.sessionToken });
+	}
+	return formattedPolicy;
 }
 
 const getEncodedPolicy = (policy) => {
-  return Buffer.from(
-    JSON.stringify(policy),
-    "utf-8"
-  ).toString("base64");
+	return Buffer.from(
+		JSON.stringify(policy),
+		"utf-8"
+	).toString("base64");
 }
 
 const getSignature = (base64EncodedPolicy, options) => {
-  return CryptoJS.HmacSHA256(
-    base64EncodedPolicy,
-    getSignatureKey(options)
-  ).toString(CryptoJS.enc.Hex);
+	return CryptoJS.HmacSHA256(
+		base64EncodedPolicy,
+		getSignatureKey(options)
+	).toString(CryptoJS.enc.Hex);
 }
 
 const getSignatureKey = (options) => {
-   const kDate = CryptoJS.HmacSHA256(options.yyyymmddDate, "AWS4" + options.secretKey);
-   const kRegion = CryptoJS.HmacSHA256(options.region, kDate);
-   const kService = CryptoJS.HmacSHA256(AWS_SERVICE_NAME, kRegion);
-   const kSigning = CryptoJS.HmacSHA256(AWS_REQUEST_POLICY_VERSION, kService);
+	const kDate = CryptoJS.HmacSHA256(options.yyyymmddDate, "AWS4" + options.secretKey);
+	const kRegion = CryptoJS.HmacSHA256(options.region, kDate);
+	const kService = CryptoJS.HmacSHA256(AWS_SERVICE_NAME, kRegion);
+	const kSigning = CryptoJS.HmacSHA256(AWS_REQUEST_POLICY_VERSION, kService);
 
-   return kSigning;
+	return kSigning;
 }
